b11778d2b4d323845be53f836b114f55b62a3e90
Lupestro <lupestro@macksoft.com>
2021-03-21 20:51:24 -0400

    Audit bumps and lint cleanups

--- tests/unit/controllers/global-test.js
+++ tests/unit/controllers/global-test.js
@@ -1,11 +1,11 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 
-module('Unit | Controller | global', function(hooks) {
+module('Unit | Controller | global', function (hooks) {
   setupTest(hooks);
 
   // TODO: Replace this with your real tests.
-  test('it exists', function(assert) {
+  test('it exists', function (assert) {
     let controller = this.owner.lookup('controller:global');
     assert.ok(controller);
   });
--- tests/unit/controllers/service-test.js
+++ tests/unit/controllers/service-test.js
@@ -1,11 +1,11 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 
-module('Unit | Controller | service', function(hooks) {
+module('Unit | Controller | service', function (hooks) {
   setupTest(hooks);
 
   // TODO: Replace this with your real tests.
-  test('it exists', function(assert) {
+  test('it exists', function (assert) {
     let controller = this.owner.lookup('controller:service');
     assert.ok(controller);
   });
